1f4d9dddddb638c93f553da758588a8f132707a4
glllom <gleb.omarov@gmail.com>
2022-05-28 13:35:10 +0300

    first commit

--- classes.py
+++ classes.py
@@ -1,47 +1,65 @@
 import os
 from file_operations import convert, create_folder
 
-JOB = r"C:\Program Files\SCM Group\Xilog Plus\Job\\"
-DOCJOB = r"C:\Users\glebo\OneDrive\Documents\landau\job\\"
+JOB = "C:\Program Files\SCM Group\Xilog Plus\Job\\"
+DOCJOB = "C:\Program Files\SCM Group\Xilog Plus\Job\\"
 
 
 class Program:
     def __init__(self):
-        self.header = 'H DX=2000.00 DY=600.00 DZ=43.00 -A C=0 T=0 R=1 *MM /"def.tlg"'
         self.gcode = ""
 
 
 class Lock(Program):
-    def __init__(self, name):
+    def __init__(self, name, short_name):
         super().__init__()
         self.height = ""
         self.name = name
-        self.path = JOB + r"templates\Lock\\" + name
+        self.short_name = short_name
+        self.path = JOB + "templates\Locks" + chr(92) + name
+        self.gcode = f'XS X=DX-1000 Y=0 Z=0 N="{self.path}"  test=test\n'
 
 
 class Hinge(Program):
-    def __init__(self, name):
+    def __init__(self, name, short_name, amount=2, symmetry=True):
         super().__init__()
-        self.height = ""
+        self.symmetry = symmetry
+        self.amount = amount
         self.name = name
-        self.path = JOB + r"templates\Hinges\\" + name
+        self.short_name = short_name
+        self.path = JOB + "templates\Hinges" + chr(92) + name
+
+    def get_code(self, amount, direction='0'):
+        return f'XS X=0 Y=0 Z=0 N="{self.path}" amount={amount} direction={direction}\n'
 
 
 class Engraving(Program):
     def __init__(self, name, group=None):
         super().__init__()
         self.name = name
-        self.path = JOB + r"templates\Engraving\\" + name
         self.group = group
+        self.path = f"{JOB}Templates{chr(92)}Engraving{chr(92)}{self.group+chr(92) if self.group else ''}{name}.pgm"
+
+    def get_code(self, flipped=0):
+        return f'XS X=0 Y=0 Z=0 N="{self.path}"  flipped={flipped}\n'
 
 
 class Door(Program):
-    def __init__(self, hinges, locks, engraving, root):
+    def __init__(self, hinges, locks, engraving, add_parts, root, dx='2020', dy='730', dz='42.5'):
         super().__init__()
         self.hinges = hinges
         self.locks = locks
         self.engraving = engraving
+        self.add_parts = add_parts
         self.root = root
+        self.header = f'H DX={dx} DY={dy} DZ={dz} -A C=0 T=0 R=100 *MM /"def.tlg"\n'
+        self.param = 'PARSECTION /"Additional"\nPAR originalDX " "\nPAR sh =5 " "\n'
+        self.gcode_checkDX = 'IF NDEF originalDX THEN\nXMSG Q=1# N="----> OriginDX !!! <----"\nFI\n'
+        self.boring = 'IF DEF originalDX THEN\nREF DX=originalDX\nFI\nXS X=0 Y=0 Z=0' \
+                      ' N="C:\PROGRAM FILES\SCM GROUP\XILOG PLUS\JOB\TEMPLATES\OTHERS\Boring_for_paint.pgm"\n'
+        self.chamfer = 'XS X=sh Y=0 Z=0 N="C:\PROGRAM FILES\SCM GROUP\XILOG PLUS\JOB\TEMPLATES\OTHERS\CHAMFER"\n'
+        self.snd_side = 'XN X=3350\nXMSG N="תכין צד שני"\nSET STANDBY =2\n'
+        self.end = 'XN X=3350\n'
 
     def create_files(self):
         os.chdir(DOCJOB)
@@ -51,14 +69,60 @@ class Door(Program):
                 create_folder(engr.group)
             create_folder(engr.name)
             for lock in self.locks:
-                create_folder(lock.name)
+                create_folder(lock.short_name)
                 for hinge in self.hinges:
-                    create_folder(hinge.name)
-                    filename = f"{engr.name}_{lock.name}_{hinge.name}.xxl"
-                    with open(filename, "w") as f:
-                        self.gcode = self.header
-                        f.write(self.gcode)
-                    convert(filename)
+                    create_folder(hinge.short_name)
+                    for amount in hinge.amount:  # different amount
+                        if hinge.symmetry:
+                            filename = f"{engr.name}_{lock.short_name}_{amount}-{hinge.short_name}.xxl"
+                            with open(filename, "w") as f:
+                                self.gcode = self.header
+                                self.gcode += self.param
+                                if self.locks is not []:
+                                    self.gcode += lock.gcode
+                                if self.hinges is not []:
+                                    self.gcode += hinge.get_code(amount)
+                                self.gcode += self.boring
+                                if "chamfer" in self.add_parts:
+                                    self.gcode += self.chamfer
+                                if self.engraving is not []:
+                                    self.gcode += engr.get_code(1)
+                                if self.engraving is not [] or "chamfer" in self.add_parts:
+                                    self.gcode += self.snd_side
+                                    if self.engraving is not []:
+                                        self.gcode += engr.get_code(2)
+                                    if "chamfer" in self.add_parts:
+                                        self.gcode += self.chamfer
+                                self.gcode += self.end
+                                f.write(self.gcode)
+                            convert(filename)
+                        else:
+                            for (index, direction) in [(1, 'R'), (2, 'L')]:
+                                filename = f"{engr.name}_{lock.short_name}_{amount}-{hinge.short_name}-{direction}.xxl"
+                                with open(filename, "w") as f:
+
+                                    self.gcode = self.header
+                                    self.gcode += self.param
+                                    if self.locks is not []:
+                                        self.gcode += lock.gcode
+                                    if self.hinges is not []:
+                                        self.gcode += hinge.get_code(amount, index)
+                                    self.gcode += self.boring
+                                    if "chamfer" in self.add_parts:
+                                        self.gcode += self.chamfer
+                                    if self.engraving is not []:
+                                        self.gcode += engr.get_code(flipped=1)
+                                    if self.engraving is not [] or "chamfer" in self.add_parts:
+                                        self.gcode += self.snd_side
+                                        if self.engraving is not []:
+                                            self.gcode += engr.get_code(flipped=2)
+                                        if "chamfer" in self.add_parts:
+                                            self.gcode += self.chamfer
+                                    self.gcode += self.end
+                                    f.write(self.gcode)
+                                convert(filename)
+
+
                     os.chdir("..")
                 os.chdir("..")
             os.chdir(DOCJOB)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from classes import Lock, Hinge, Engraving, Door
 
-lock_magnet = Lock("Lock_Magnet")
-Lock_AGB_EN12209 = Lock("Lock_AGB_EN12209")
+lock_magnet = Lock("Lock_Magnet", "מגנט")
+Lock_AGB_EN12209 = Lock("Lock_AGB_EN12209", "AGB")
 
-hinge_cemom = Hinge("Hinge_Cemom-80")
-hinge_luz = Hinge("Hinge_LUZ")
-hinge_simonswerk240 = Hinge("Hinge_s240")
+hinge_cemom = Hinge("Cemom_80.pgm", "צרפתי-80", [2, 3])
+hinge_luz = Hinge("LUZ", "LUZ", [2, 3], False)
+hinge_simonswerk340 = Hinge("Simonswerk_340.pgm", "s340", [2, 3])
 eng_lst = [Engraving("N01_1P20mm", "N01_P20mm"),
            Engraving("N01_2P20mm", "N01_P20mm"),
            Engraving("N02_1P_imm", "N02_P_imm"),
@@ -20,11 +20,18 @@ eng_lst = [Engraving("N01_1P20mm", "N01_P20mm"),
            Engraving("N14_1P_8mm", "N14_P_8mm"),
            Engraving("N14_2P_8mm", "N14_P_8mm"),
            Engraving("N14_2Ph_8mm", "N14_P_8mm"),
-           Engraving("N21_123123123"),
-           Engraving("N22_123123123"),
-           Engraving("N23_123123123"),
+           Engraving("N21_v400"),
+           Engraving("N22_v14248"),
+           Engraving("N23_v401050"),
+           Engraving("N24_"),
+           Engraving("N25_v6050148"),
+           Engraving("N26_v145060"),
+           Engraving("N27_v2323"),
+           Engraving("N28_v1423"),
+           Engraving("N29_"),
            ]
 
-door_landau = Door(hinges=[hinge_cemom, hinge_luz, hinge_simonswerk240], locks=[lock_magnet, Lock_AGB_EN12209],
-                   engraving=eng_lst, root="3.דלתות לנדאו")
+door_landau = Door(hinges=[hinge_cemom, hinge_luz, hinge_simonswerk340],
+                   locks=[lock_magnet, Lock_AGB_EN12209],
+                   engraving=eng_lst, add_parts=("chamfer"), root="3.דלתות לנדאו")
 door_landau.create_files()
